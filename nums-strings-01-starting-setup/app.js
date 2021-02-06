9c8c283116353ae9b2aee00f1b34415dd1507403
Jose <gauranajose@gmail.com>
2021-02-06 14:44:18 +0800

    numbers & strings

---
+++ nums-strings-01-starting-setup/app.js
@@ -0,0 +1,23 @@
+function randomIntBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(randomIntBetween(1,10));
+
+function productDescription(strings, productName, productPrice) {
+    console.log(strings);
+    console.log(productName);
+    console.log(productPrice);
+    let priceCategory = 'pretty cheap';
+    if (productPrice > 20) {
+        priceCategory = 'fairly priced';
+    }
+    return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+}
+
+const prodName = 'JavaScript Course';
+
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
